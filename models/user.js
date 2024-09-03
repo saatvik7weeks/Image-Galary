cca034c3b0f724cbbf25a5dcd325378b2a06b37e
saatvik7weeks <118299840+saatvik7weeks@users.noreply.github.com>
2024-09-04 02:32:55 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-//password is skpsmp
-const mongourl='mongodb+srv://rishabpanda779:skpsmp@cluster0.sp8sj.mongodb.net/'
+//password without @123
+const mongourl='mongodb+srv://rishabpanda779:******@cluster0.sp8sj.mongodb.net/'
 
 mongoose.connect(mongourl, {
     useNewUrlParser: true,
